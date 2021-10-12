3b9a5cb6aa4603bdf7ecb10bebbc4fb5c35f60d8
mbl9898 <muhammadbutt70@gmail.com>
2021-10-12 09:58:10 +0500

    Feat: Update Authorized Votes

---
+++ backEnd/controllers/authorized/helpers/updateVoteData.js
@@ -0,0 +1,101 @@
+import { logger } from '~/utils';
+import { status } from '~/constants';
+import { AuthorizedSchema } from '~/schemas';
+
+export const updateVoteData = async (req, res) => {
+  const { OK, SERVER_ERROR } = status;
+  try {
+    if (!req.body.cnic) {
+      throw new Error('Invalid CNIC Number');
+    }
+    const {
+      blockCode,
+      voteSNo,
+      familyNo,
+      gender,
+      name,
+      fatherHusbandName,
+      maritalStatus,
+      cnic,
+      age,
+      houseNo,
+      street,
+      phase,
+      sector,
+      lane,
+      boulevardAvenue,
+      otherArea,
+      constituencyName,
+      moza,
+      dehya,
+      city,
+      patwarHalka,
+      tapaydar,
+      tehseel,
+      talka,
+      district,
+      unionCouncil,
+      bookNo,
+      constituency,
+      address,
+      enteredBy,
+    } = req.body;
+
+    const verifiedBy = req.user;
+
+    const data = await UnAuthorizedSchema.findByIdAndUpdate(
+      { _id: req.body._id },
+      {
+        blockCode,
+        voteSNo,
+        familyNo,
+        gender,
+        name,
+        fatherHusbandName,
+        maritalStatus,
+        cnic,
+        age,
+        houseNo,
+        street,
+        phase,
+        sector,
+        lane,
+        boulevardAvenue,
+        otherArea,
+        constituencyName,
+        moza,
+        dehya,
+        city,
+        patwarHalka,
+        tapaydar,
+        tehseel,
+        talka,
+        district,
+        unionCouncil,
+        bookNo,
+        constituency,
+        address,
+        enteredBy,
+        verifiedBy: {
+          username: verifiedBy.username,
+          email: verifiedBy.email,
+          role: verifiedBy.role,
+        },
+        LastUpdatedAt: new Date().toISOString(),
+      },
+      { new: true },
+    );
+
+    return res.json({
+      success: true,
+      data,
+    });
+  } catch (e) {
+    logger('error', 'Error:', e.message);
+    return res.json({
+      status: SERVER_ERROR,
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+};

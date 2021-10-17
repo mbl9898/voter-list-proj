031195dcb2e13a6ec44b7d785faec4c992d24cc7
mbl9898 <muhammadbutt70@gmail.com>
2021-10-17 09:46:55 +0500

    Improve: Search Field Added and filter on its basis

--- backEnd/controllers/authorized/helpers/searchAuthorizedVotes.js
+++ backEnd/controllers/authorized/helpers/searchAuthorizedVotes.js
@@ -6,6 +6,7 @@ export const searchAuthorizedVotes = async (req, res) => {
   const { SERVER_ERROR } = status;
   try {
     const filteredData = [];
+    const searchField = req.query.searchField;
     const searchTerm = req.query.search.toString().toLowerCase().trim();
     const data = await AuthorizedSchema.find();
     if (!data[0]) {
@@ -16,52 +17,51 @@ export const searchAuthorizedVotes = async (req, res) => {
       });
     }
     console.log(searchTerm);
-    let voteKeys = [];
-    Object.entries(data[0]._doc).forEach(([key, value]) => {
-      voteKeys.push(key);
-    }),
-      (voteKeys = voteKeys.filter(
-        (key) =>
-          key !== '_id' &&
-          key !== 'enteredBy' &&
-          key !== 'createdAt' &&
-          key !== 'verifiedBy' &&
-          key !== '__v',
-      ));
+    // let voteKeys = [];
+    // Object.entries(data[0]._doc).forEach(([key, value]) => {
+    //   voteKeys.push(key);
+    // }),
+    //   (voteKeys = voteKeys.filter(
+    //     (key) =>
+    //       key !== '_id' &&
+    //       key !== 'enteredBy' &&
+    //       key !== 'createdAt' &&
+    //       key !== 'verifiedBy' &&
+    //       key !== '__v',
+    //   ));
 
     if (searchTerm) {
       filteredData.push(
         ...data.filter((x) => {
-          return (
-            x[voteKeys[0]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[1]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[2]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[3]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[4]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[5]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[6]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[7]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[8]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[9]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[10]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[11]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[12]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[13]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[14]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[15]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[16]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[17]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[18]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[19]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[20]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[21]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[22]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[23]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[24]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[25]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[26]]?.toString().toLowerCase().includes(searchTerm) ||
-            x[voteKeys[27]]?.toString().toLowerCase().includes(searchTerm)
-          );
+          return x[searchField]?.toString().toLowerCase().includes(searchTerm);
+          // ||
+          // x[voteKeys[1]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[2]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[3]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[4]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[5]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[6]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[7]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[8]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[9]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[10]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[11]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[12]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[13]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[14]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[15]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[16]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[17]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[18]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[19]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[20]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[21]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[22]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[23]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[24]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[25]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[26]]?.toString().toLowerCase().includes(searchTerm) ||
+          // x[voteKeys[27]]?.toString().toLowerCase().includes(searchTerm)
         }),
       );
     } else {
